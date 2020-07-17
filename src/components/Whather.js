e098b6053bb5f93562a8254a3c943a12720aef2a
OmarHassin <boky555555@gmail.com>
2020-07-17 23:50:12 +0200

    Weather-App

--- src/components/Whather.js
+++ src/components/Whather.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Weather extends Component {
+class Weather extends Component {
   render() {
     return (
       <div>
@@ -9,3 +9,5 @@ export default class Weather extends Component {
     );
   }
 }
+
+export default Weather;
